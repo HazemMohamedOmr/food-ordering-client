a5379d8168fc2e98b8c109b12aa6139f70cf98db
Hazem Mohamed Omar <55815459+HazemMohamedOmr@users.noreply.github.com>
2025-06-27 06:45:37 +0300

    use live

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7135/api'
-};
-//  export const environment = {
+// export const environment = {
 //   production: false,
-//   apiUrl: 'http://fod.runasp.net/api'
-// }; 
+//   apiUrl: 'https://localhost:7135/api'
+// };
+ export const environment = {
+  production: false,
+  apiUrl: 'http://fod.runasp.net/api'
+}; 
