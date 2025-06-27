b2993461b3010f6d824d8eeac16cd04373db9206
Hazem Mohamed Omar <55815459+HazemMohamedOmr@users.noreply.github.com>
2025-06-27 21:59:12 +0300

    Auto-generated commit

---
+++ chunk-DUY76TJX.js
@@ -0,0 +1,1 @@
+import"./chunk-AIZVJUQQ.js";var o=[{path:"",loadComponent:()=>import("./chunk-WKSJ5G46.js").then(t=>t.RestaurantListComponent),title:"Restaurants - QualiFoodie"},{path:":id",loadComponent:()=>import("./chunk-4LVSRNDY.js").then(t=>t.RestaurantDetailComponent),title:"Restaurant Details - QualiFoodie"}];export{o as RESTAURANTS_ROUTES};
